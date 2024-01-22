b30c0ba0a3fa726d50294261c76fb4e01e279fce
Jaime Leon Holgado <jleonhol@gmail.com>
2024-01-22 14:59:49 +0000

    Added custom CreateWorkspace and RunCombine tasks

--- tasks/analysis.py
+++ tasks/analysis.py
@@ -1,7 +1,9 @@
 import json
 import luigi
 
-from cmt.base_tasks.analysis import Fit, CreateDatacards, CombineDatacards
+from cmt.base_tasks.analysis import (
+    Fit, CreateDatacards, CombineDatacards, CreateWorkspace, RunCombine
+)
 
 
 class CreateDatacardsDQCD(CreateDatacards):
@@ -45,7 +47,8 @@ class CreateDatacardsDQCD(CreateDatacards):
 
     def run(self):
         inputs = self.input()
-        assert self.feature_names == ("muonSV_bestchi2_mass",)
+        assert self.feature_names[0].startswith("muonSV_bestchi2_mass") and \
+            len(self.feature_names) == 0
         with open(inputs["tight"]["muonSV_bestchi2_mass"]["json"].path) as f:
             d_tight = json.load(f)
         with open(inputs["loose"]["muonSV_bestchi2_mass"]["json"].path) as f:
@@ -60,13 +63,30 @@ class CreateDatacardsDQCD(CreateDatacards):
 class CombineDatacardsDQCD(CombineDatacards):
     fit_config_file = luigi.Parameter(default="fit_config", description="file including "
         "fit configuration, default: fit_config.yaml")
+    category_name = "base"
+    category_names = ("base",)
+
+    def __init__(self, *args, **kwargs):
+        super(CombineDatacardsDQCD, self).__init__(*args, **kwargs)
+        self.fit_config = self.config.get_fit_config(self.fit_config_file)
+        self.category_names = self.fit_config[self.process_group_name].keys()
 
     def requires(self):
-        d = self.config.get_fit_config(self.fit_config_file)
-        self.category_names = d[self.process_group_name].keys()
         return {
             category_name: CreateDatacardsDQCD.vreq(self, category_name=category_name,
-                counting=d[self.process_group_name][category_name],
-                _exclude=["category_names", "log_file"])
+                counting=self.fit_config[self.process_group_name][category_name],
+                _exclude=["category_names"])
             for category_name in self.category_names
         }
+
+
+class CreateWorkspaceDQCD(CreateWorkspace, CombineDatacardsDQCD):
+    def requires(self):
+        return CombineDatacardsDQCD.vreq(self)
+
+
+class RunCombineDQCD(RunCombine, CreateWorkspaceDQCD):
+    method = "limits"
+
+    def requires(self):
+        return CreateWorkspaceDQCD.vreq(self)
