631184beadb2414476982d3e08fdaa43a82298d0
Jaime Leon Holgado <jleonhol@gmail.com>
2024-01-23 13:18:09 +0000

    Added ScanCombineDQCD task and some fixes in the code and changes in how the scaling is done for the background

--- tasks/analysis.py
+++ tasks/analysis.py
@@ -1,5 +1,7 @@
 import json
 import luigi
+import law
+from copy import deepcopy as copy
 
 from cmt.base_tasks.analysis import (
     Fit, CreateDatacards, CombineDatacards, CreateWorkspace, RunCombine
@@ -14,44 +16,57 @@ class CreateDatacardsDQCD(CreateDatacards):
     mass_point = luigi.FloatParameter(default=1.33, description="mass point to be used to "
         "define the fit ranges and blinded regions")
 
+    calibration_feature_name = "muonSV_bestchi2_mass_fullrange"
+
     def __init__(self, *args, **kwargs):
         super(CreateDatacardsDQCD, self).__init__(*args, **kwargs)
         assert "tight" in self.region_name
+        assert len(self.config.process_group_names[self.process_group_name]) == 3
+        for process in self.config.process_group_names[self.process_group_name]:
+            if self.config.processes.get(process).isSignal:
+                signal_process = process
+                break
+        for model in self.models:
+            process_name = self.models[model]["process_name"]
+            if process_name != signal_process and self.config.processes.get(process_name).isSignal:
+                self.models[model]["process_name"] = signal_process
+                self.models[signal_process] = copy(self.models[model])
+                del self.models[model]
 
     def requires(self):
         reqs = CreateDatacards.requires(self)
         sigma = self.mass_point * 0.01
         fit_range = (self.mass_point - 5 * sigma, self.mass_point + 5 * sigma)
 
-        if not self.counting:
-            loose_region = self.region.name.replace("tight", "loose")
-            for process in reqs["fits"]:
-                reqs["fits"][process].x_range = fit_range
-                if process == "background":
-                    reqs["fits"][process].region_name = loose_region
-                else:
-                    reqs["fits"][process].region_name = self.region_name
+        loose_region = self.region.name.replace("tight", "loose")
+        for process in reqs["fits"]:
+            reqs["fits"][process].x_range = fit_range
+            if process == "background":
+                reqs["fits"][process].region_name = loose_region
+            else:
+                reqs["fits"][process].region_name = self.region_name  # probably redundant
 
-                import yaml
-                from cmt.utils.yaml_utils import ordered_load
-                with open(self.retrieve_file("config/{}.yaml".format(self.fit_models))) as f:
-                    self.models = ordered_load(f, yaml.SafeLoader)
-                for model, fit_params in self.models.items():
-                    fit_params["x_range"] = str(fit_range)[1:-1]
-                    if fit_params["process_name"] == "background":
-                        params = ", ".join([f"{param}='{value}'"
-                            for param, value in fit_params.items() if param != "fit_parameters"])
-                        if "fit_parameters" in fit_params:
-                            params += ", fit_parameters={" + ", ".join([f"'{param}': '{value}'"
-                            for param, value in fit_params["fit_parameters"].items()]) + "}"
+            for model, fit_params in self.models.items():
+                fit_params["x_range"] = str(fit_range)[1:-1]
+                if fit_params["process_name"] == "background":
+                    params = ", ".join([f"{param}='{value}'"
+                        for param, value in fit_params.items() if param != "fit_parameters"])
+                    if "fit_parameters" in fit_params:
+                        params += ", fit_parameters={" + ", ".join([f"'{param}': '{value}'"
+                        for param, value in fit_params["fit_parameters"].items()]) + "}"
 
-                            reqs["tight"] =  eval(f"Fit.vreq(self, {params}, _exclude=['include_fit'], "
-                                "region_name=self.tight_region, feature_names=self.feature_names, "
-                                "category_name='base')")
-                            reqs["loose"] =  eval(f"Fit.vreq(self, {params}, _exclude=['include_fit'], "
-                                "region_name=self.loose_region, feature_names=self.feature_names,"
-                                "category_name='base')")
-        else:  # counting
+                        reqs["tight"] =  eval("Fit.vreq(self,version=self.version + '_calib', "
+                            f"{params}, _exclude=['include_fit'], "
+                            "region_name=self.tight_region, "
+                            f"feature_names=('{self.calibration_feature_name}',), "
+                            "category_name='base')")
+                        reqs["loose"] =  eval(f"Fit.vreq(self, version=self.version + '_calib', "
+                            f"{params}, _exclude=['include_fit'], "
+                            "region_name=self.loose_region, "
+                            f"feature_names=('{self.calibration_feature_name}',), "
+                            "category_name='base')")
+
+        if self.counting:  # counting
             blind_range = (str(self.mass_point - 2 * sigma), str(self.mass_point + 2 * sigma))
             for process in reqs["fits"]:
                 if process == "background":
@@ -59,28 +74,26 @@ class CreateDatacardsDQCD(CreateDatacards):
                     reqs["fits"][process].blind_range = blind_range
                 else:
                     reqs["fits"][process].x_range = blind_range
+
         return reqs
 
     def run(self):
         inputs = self.input()
-        assert self.feature_names[0].startswith("muonSV_bestchi2_mass") and \
-            len(self.feature_names) == 1
+        with open(inputs["tight"][self.calibration_feature_name]["json"].path) as f:
+            d_tight = json.load(f)
+        with open(inputs["loose"][self.calibration_feature_name]["json"].path) as f:
+            d_loose = json.load(f)
+        additional_scaling = d_tight[""]["integral"] / d_loose[""]["integral"]
 
         if not self.counting:
-            with open(inputs["tight"]["muonSV_bestchi2_mass"]["json"].path) as f:
-                d_tight = json.load(f)
-            with open(inputs["loose"]["muonSV_bestchi2_mass"]["json"].path) as f:
-                d_loose = json.load(f)
-
-            self.additional_scaling = {"background": d_tight[""]["integral"] /\
-                d_loose[""]["integral"]}
+            self.additional_scaling = {"background": additional_scaling}
         else:
-            self.additional_scaling = {"background": 2/3}
+            self.additional_scaling = {"background": additional_scaling * 2/3}
 
         super(CreateDatacardsDQCD, self).run()
 
 
-class CombineDatacardsDQCD(CombineDatacards):
+class CombineDatacardsDQCD(CombineDatacards, CreateDatacardsDQCD):
     fit_config_file = luigi.Parameter(default="fit_config", description="file including "
         "fit configuration, default: fit_config.yaml")
     category_name = "base"
@@ -110,3 +123,38 @@ class RunCombineDQCD(RunCombine, CreateWorkspaceDQCD):
 
     def requires(self):
         return CreateWorkspaceDQCD.vreq(self)
+
+
+class ScanCombineDQCD(RunCombineDQCD):
+    process_group_names = law.CSVParameter(default=(), description="process_group_names to be used, "
+        "empty means all scenarios, default: empty")
+    process_group_name = "scenarioA_mpi_4_mA_1p33_ctau_10"  # placeholder
+
+    def __init__(self, *args, **kwargs):
+        super(ScanCombineDQCD, self).__init__(*args, **kwargs)
+        if not self.process_group_names:
+            process_group_names = [group for group in self.config.process_group_names
+                if group.startswith("scenario")]
+            self.process_group_names = []
+            for process_group_name in process_group_names:
+                if not process_group_name in self.fit_config:
+                    print(f"{process_group_name} will be skipped since it is not in the fit config "
+                        "provided" )
+                    continue
+                self.process_group_names.append(process_group_name)
+
+    def requires(self):
+        reqs = {}
+        for process_group_name in self.process_group_names:
+            i = process_group_name.find("mA_")
+            f = process_group_name.find("_ctau")
+            mass_point = float(process_group_name[i + 3:f].replace("p", "."))
+            reqs[process_group_name] = RunCombineDQCD.vreq(self, mass_point=mass_point,
+                process_group_name=process_group_name)
+        return reqs
+
+    def complete(self):
+        return False
+
+    def run(self):
+        pass
